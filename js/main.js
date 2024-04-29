658dd5eb3c65696bc36a8bebc106fc9ef9dd52a4
RiJaba1 <rijaba1@protonmail.com>
2024-04-29 22:38:26 +0200

    Arreglo del selector por SO

--- js/main.js
+++ js/main.js
@@ -1,52 +1,51 @@
-const selectContainers = document.querySelectorAll('.select-container');
-const selectOptions = document.querySelectorAll('.select-options');
+// Función para manejar la lógica de selección/deselección de "Todas las opciones"
+function updateAllOption(optionsList, allOption) {
+    const anySelected = optionsList.some(option => option.checked);
+    allOption.checked = !anySelected;
+}
 
-selectContainers.forEach((selectContainer, index) => {
-    const options = selectOptions[index].querySelectorAll('.option');
-    const AllOption = options[0].querySelector('input[type="checkbox"]');
-    const otherOptions = Array.from(options).slice(1);
-
-    selectContainer.addEventListener('click', () => {
-        selectOptions[index].classList.toggle('active');
-    });
-
-    function updateAllOption() {
-        const isAllSelected = AllOption.checked;
-        const anyOtherSelected = otherOptions.some(option => option.querySelector('input[type="checkbox"]').checked);
-
-        if (isAllSelected && anyOtherSelected) {
-            AllOption.checked = false;
-        } else if (!isAllSelected && !anyOtherSelected) {
-            AllOption.checked = true;
-        }
-    }
-
-    options.forEach((option) => {
+// Función para inicializar la lógica de selección/deselección de "Todas las opciones"
+function initAllOption(optionsList, allOption) {
+    optionsList.forEach(option => {
         option.addEventListener('click', () => {
-            const optionValue = option.querySelector('input[type="checkbox"]').value;
-
-            if (optionValue === 'all') {
-                // Cuando se hace clic en "Todas las plataformas", desmarcar todas las demás
-                otherOptions.forEach((otherOption) => {
-                    otherOption.querySelector('input[type="checkbox"]').checked = false;
-                });
-                // También selecciona automáticamente "Todos los SO"
-                document.getElementById('so-filter-options').querySelector('.all-option input[type="checkbox"]').checked = true;
-            } else {
-                // Cuando se hace clic en cualquier otra opción, desmarcar "Todas las plataformas"
-                AllOption.checked = false;
-            }
-
-            // Actualizar el estado del checkbox "Todas las plataformas"
-            updateAllOption();
-
-            const selectedPlatforms = Array.from(document.querySelectorAll('#platform-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
-            const selectedSOs = Array.from(document.querySelectorAll('#so-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
-            filterCtf(selectedPlatforms, selectedSOs);
+            updateAllOption(optionsList, allOption);
         });
+    });
+}
+
+const platformOptions = document.querySelectorAll('#platform-filter-options input[type="checkbox"]');
+const platformAllOption = platformOptions[0];
+const platformOtherOptions = Array.from(platformOptions).slice(1);
+
+initAllOption(platformOtherOptions, platformAllOption);
+
+const SOOptions = document.querySelectorAll('#so-filter-options input[type="checkbox"]');
+const SOAllOption = SOOptions[0];
+const SOOtherOptions = Array.from(SOOptions).slice(1);
+
+initAllOption(SOOtherOptions, SOAllOption);
+
+// Event listener para la lista de plataformas
+platformOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        updateAllOption(platformOtherOptions, platformAllOption);
+        const selectedPlatforms = Array.from(document.querySelectorAll('#platform-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
+        const selectedSOs = Array.from(document.querySelectorAll('#so-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
+        filterCtf(selectedPlatforms, selectedSOs);
     });
 });
 
+// Event listener para la lista de SO
+SOOptions.forEach(option => {
+    option.addEventListener('click', () => {
+        updateAllOption(SOOtherOptions, SOAllOption);
+        const selectedPlatforms = Array.from(document.querySelectorAll('#platform-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
+        const selectedSOs = Array.from(document.querySelectorAll('#so-filter-options input[type="checkbox"]:checked'), (checkbox) => checkbox.value);
+        filterCtf(selectedPlatforms, selectedSOs);
+    });
+});
+
+// Función de filtro
 function filterCtf(platforms, SOs) {
     let elements = document.querySelectorAll(".card");
     elements.forEach((element) => {
